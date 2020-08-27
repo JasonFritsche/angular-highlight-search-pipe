b9f7d4ec72efc70ba9944cc0396ec80b7562a9e9
jasonf <jasonf@pop-os.localdomain>
2020-08-27 17:50:39 -0500

    added highlight pipe

---
+++ src/app/directives/search-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SearchHighlightDirective } from './search-highlight.directive';
+
+describe('SearchHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SearchHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/search-highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appSearchHighlight]'
+})
+export class SearchHighlightDirective {
+
+  constructor() { }
+
+}
